4d3a2f0d6154ee46bff4743e24222df31906cd73
Mr_gao <gxlself@gmail.com>
2019-07-10 12:04:48 +0800

    ‘完成数据存储以及提取， 下一步添加登录授权状态框

--- pages/add-choice/add-choice.js
+++ pages/add-choice/add-choice.js
@@ -8,24 +8,122 @@ Page({
     editChoice: [{
       content: '',
       isConfirm: false
-    }]
+    }],
+    currentTitle: '',
+    openId: ''
   },
   back() {
     wx.navigateBack()
   },
+  // 键盘下一条点击
   nextInput(e) {
+    let index = e.currentTarget.dataset.index
+    // 前后去空判定
     if (e.detail.value == '' || e.detail.value == ' ' || e.detail.value.trim() == '' || e.detail.value.trim() == ' ') {
-      wx.showToast({title: '请填写当前选项',icon: 'none',duration: 1500})
+      wx.showToast({title: '请填写当前选项',icon: 'none',duration: 1500, mask: true})
       return
     }
-    let index = this.data.editChoice.length - 1
-    this.data.editChoice[index].content = e.detail.value.trim()
-    this.data.editChoice[index].isConfirm = true
-    this.data.editChoice.push({content: '',isConfirm: false})
+    // 对是否编辑进行判定 index不是最后一条就属于编辑状态
+    if (index != this.data.editChoice.length - 1) {
+      this.data.editChoice[index].content = e.detail.value.trim()
+      this.data.editChoice[index].isConfirm = true
+    } else {
+      let addIndex = this.data.editChoice.length - 1
+      this.data.editChoice[addIndex].content = e.detail.value.trim()
+      this.data.editChoice[addIndex].isConfirm = true
+      this.data.editChoice.push({content: '',isConfirm: false})
+    }
     this.setData({editChoice: this.data.editChoice})
   },
+  // 编辑已经确认的
+  editChoice(e) {
+    let index = e.currentTarget.dataset.index
+    this.data.editChoice[index].isConfirm = false
+    this.setData({editChoice: this.data.editChoice})
+  },
+  // 标题输入绑定
+  inputTitle(e) {
+    this.data.currentTitle = e.detail.value
+    this.setData({currentTitle: this.data.currentTitle})
+  },
+  // 对应输入绑定
+  inputPointContent(e) {
+    let index = e.currentTarget.dataset.index
+    this.data.editChoice[index].content = e.detail.value.trim()
+    this.setData({editChoice: this.data.editChoice})
+  },
+  // 开始保存当前疑难
+  saveChoice() {
+    if (this.data.currentTitle == '' || this.data.currentTitle == ' ' || this.data.currentTitle.trim() == '' || this.data.currentTitle.trim() == ' ') {
+      wx.showToast({title: '请填写犹豫的主题',icon: 'none',duration: 1500, mask: true})
+      return
+    }
+    let length = this.data.editChoice.length
+    if (length == 1) {
+      if (this.checkContent(this.data.editChoice[0].content)) {
+        this.formChoice(true)
+      } else {
+        wx.showToast({title: '没有犹豫的事项',icon: 'none',duration: 1500, mask: true})
+      }
+    } else if (length > 1) {
+      let isSaveLast = false
+      if (this.checkContent(this.data.editChoice[length - 1].content)) {
+        // 对话框判定是否保存最后一条
+        wx.showModal({
+          title: '是否保存最后一条',
+          showCancel: true,
+          cancelText: '不保存',
+          confirmText: '保存',
+          success: res => {
+            if (res.confirm) {
+              this.formChoice(true)
+            } else {
+              this.formChoice(false)
+            }
+          }
+        })
+        return
+      }
+    } else {
+      wx.showToast({title: '请添加犹豫的事项', icon: 'none', duration: 1500, mask: true})
+      return
+    }
+  },
+  // 简单校验
+  checkContent(content) {
+    if (content == '' || content == ' ' || content.trim() == '' || content.trim() == ' ') {
+      return false
+    } else {
+      return true
+    }
+  },
+  // 提交
+  formChoice(saveLast) {
+    let choice = this.data.editChoice
+    if (!saveLast) {
+      this.data.editChoice.pop()
+      choice = this.data.editChoice
+    }
+    gxl.addData('choice', {
+      title: this.data.currentTitle,
+      choice: choice
+    }, res => {
+      this.back()
+    }, err => {
+      wx.showToast({title: '添加失败',icon: 'none',duration: 1500, mask: true})
+    })
+  },
   onLoad: function (options) {
-
+    gxl.getStorage('openid', res => {
+      if (res){
+        console.log(res)
+        this.setData({ isLogin: true, openId: res})
+      }else{
+        this.setData({ isLogin: false })
+      }
+    }, err => {
+      this.setData({isLogin: false})
+    })
   },
   onReady: function () {
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,9 @@ Page({
   data: {
     DEFAULT_HEADER_HEIGHT: wx.DEFAULT_HEADER_HEIGHT,
     STATUS_BAR_HEIGHT: wx.STATUS_BAR_HEIGHT,
-    BODY_HEIGHT: wx.WIN_HEIGHT - wx.STATUS_BAR_HEIGHT - wx.DEFAULT_HEADER_HEIGHT
+    BODY_HEIGHT: wx.WIN_HEIGHT - wx.STATUS_BAR_HEIGHT - wx.DEFAULT_HEADER_HEIGHT,
+    openId: '',
+    choiceList: []
   },
   onShareAppMessage: function (e) {
     console.log(e)
@@ -17,12 +19,18 @@ Page({
   },
   onLoad: function () {
     console.log(app.globalData)
+    
   },
   onShow: function(){
     let that = this;
-    let openid = gxl.getStorage('openid', res => {
+    gxl.getStorage('openid', res => {
       if (res){
-        that.setData({ isLogin: true })
+        that.setData({ isLogin: true, openId: res })
+        gxl.getMoreData('choice', {_openid: res}, res =>{
+          if (res.errMsg.indexOf('get:ok') > -1) {
+            that.setData({choiceList: res.data})
+          }
+        })
       }else{
         that.setData({ isLogin: false })
       }
